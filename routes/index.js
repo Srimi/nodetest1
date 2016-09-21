e43d945d6454b1b8b1d5dbde6cd9f0c9d45009eb
SRIRAM B <sriramb@VISUALBI.LOCAL>
2016-09-21 15:59:30 +0530

    fixed wsdl

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,19 @@ var soap = require('soap');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  var url = 'http://www.webservicex.net/country.asmx?WSDL';
+  var url = 'http://www.webservicex.net/geoipservice.asmx?WSDL';
   var args = {};
   var response = "";
   soap.createClient(url, function(err, client) {
-      client.GetCountries(args, function(err, result) {
-      	response  = String(result);
-      	console.log('type',typeof(response));
-        console.log(response);
-        res.render('index', { title: result });
+      client.GetGeoIPContext(args, function(err, result) {
+        // response  = result;
+        res_obj = JSON.stringify(result)
+        console.log('type',typeof(res_obj));
+        console.log(res_obj);
+        res.render('index', { title: res_obj });
       });
   });
-  console.log("r",response)
+  // console.log("r",response)
   // res.render('index', { title: response });
 });
 
